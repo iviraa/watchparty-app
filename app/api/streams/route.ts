2bc3681f1d46f24b482c46e45c7cee362f7f4b82
iviraa <chetanchal.cs@gmail.com>
2024-09-22 06:13:25 -0500

    add thumbnails and titles to stream

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,6 +2,9 @@ import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
+
 var YT_REGEX =
   /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 
@@ -28,12 +31,26 @@ export async function POST(req: NextRequest) {
 
     const extractedId = data.url.split("?v=")[1];
 
+    const res = await youtubesearchapi.GetVideoDetails(extractedId);
+    console.log(res.title);
+    console.log(res.thumbnail.thumbnails);
+    const thumbnails = res.thumbnail.thumbnails;
+    thumbnails.sort((a: { width: number }, b: { width: number }) =>
+      a.width < b.width ? -1 : 1
+    );
+
     const stream = await prismaClient.stream.create({
       data: {
         userId: data.creatorId,
         url: data.url,
         extractedId,
         type: "Youtube",
+        title: res.title ?? "",
+        smallImg:
+          (thumbnails.length > 1
+            ? thumbnails[thumbnails.length - 2].url
+            : thumbnails[thumbnails.length - 1].url) ?? "",
+        bigImg: thumbnails[thumbnails.length - 1].url ?? "",
       },
     });
 
